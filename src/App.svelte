badaf38be60a20a6ad2d57f035631c655b9c52cd
ootidea <ootidea@gmail.com>
2021-10-25 17:23:17 +0900

    Counterコンポーネントを最低限実装

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,16 @@
 <script lang="ts">
+  import Counter from './Counter.svelte'
+
+  type CounterState = { count: number; title: string }
+
+  const counterStates: CounterState[] = [{ count: 0, title: '' }]
 </script>
 
-<main />
+<main>
+  {#each counterStates as counterState}
+    <Counter bind:count={counterState.count} bind:title={counterState.title} />
+  {/each}
+</main>
 
 <style>
 </style>
